807a4b733285cc46a716eb004b01e3ab5a3fac21
ScottFreeCode <ScottFreeCode@users.noreply.github.com>
2016-11-01 23:43:11 -0400

    Make comment significantly more helpful

--- example/assert.js
+++ example/assert.js
@@ -1,4 +1,6 @@
-// for the browser
+// Normally, you would pick a browser-compatible assertion library.
+// For this example, the assertion is mostly demonstrating use of AMD
+// dependencies; we can use a simplistic equivalent in the browser.
 define(function() { return function assert(value) {
   if (!value) { throw new Error("Assertion failed!") }
 }})
